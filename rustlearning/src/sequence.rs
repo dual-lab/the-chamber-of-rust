df6d3dec04d606563855dd0aa7588b84b909ae5f
dmike16 <cipmiky@gmail.com>
2025-03-08 23:59:25 +0100

    feat(rustlearning): add comment

--- rustlearning/src/sequence.rs
+++ rustlearning/src/sequence.rs
@@ -1,11 +1,14 @@
 use std::collections::HashMap;
-
+/// Find the median point of an ordered sequence of integer
+///
 pub fn find_median(seq: &[i32]) -> Option<i32> {
     let mut cseq = Vec::from(seq);
     cseq.sort_unstable();
     cseq.get(cseq.len() / 2).copied()
 }
 
+/// Find the freq with which integer appair int the sequence
+///
 pub fn find_mode(seq: &[i32]) -> HashMap<i32,i32> {
     let mut mode: HashMap<i32, i32> = HashMap::new();
     for z in seq {
